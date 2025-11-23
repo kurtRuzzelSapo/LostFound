7a1c17cb39c1719baefe3f5da47b0de77e1af88a
kurtRuzzelSapo <wkurtruzzelsapo@gmail.com>
2025-11-23 15:00:15 +0800

    Remove unused PWA configuration and service worker code from project

--- vite.config.ts
+++ vite.config.ts
@@ -2,39 +2,39 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
+// import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
-   plugins: [
+  plugins: [
     react(),
     tailwindcss(),
-    VitePWA({
-      registerType: "autoUpdate",
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-      },
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
-      manifest: {
-        name: "FindIt",
-        short_name: "FindIt",
-        description: "Lost and Found Item Management Application",
-        theme_color: "#00ae00ff",
-        background_color: "#ffffff",
-        display: "standalone",
-        start_url: "/",
-        icons: [
-          {
-            src: "/icons/FI_logo.ico",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/icons/FI_logo.ico",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
+    // VitePWA({
+    //   registerType: "autoUpdate",
+    //   workbox: {
+    //     globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+    //   },
+    //   includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+    //   manifest: {
+    //     name: "FindIt",
+    //     short_name: "FindIt",
+    //     description: "Lost and Found Item Management Application",
+    //     theme_color: "#00ae00ff",
+    //     background_color: "#ffffff",
+    //     display: "standalone",
+    //     start_url: "/",
+    //     icons: [
+    //       {
+    //         src: "/icons/FI_logo.ico",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "/icons/FI_logo.ico",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //       },
+    //     ],
+    //   },
+    // }),
   ],
   resolve: {
     alias: {
