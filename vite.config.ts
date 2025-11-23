ba98983b300a45818feb87a8c4a729cfaaa9b886
kurtRuzzelSapo <wkurtruzzelsapo@gmail.com>
2025-11-23 12:54:19 +0800

    Add manifest.json for PWA configuration with icons and app details

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,40 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-
-// https://vite.dev/config/
+import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+   plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "FindIt",
+        short_name: "FindIt",
+        description: "Lost and Found Item Management Application",
+        theme_color: "#00ae00ff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/icons/FI_logo.ico",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/FI_logo.ico",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
